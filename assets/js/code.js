8727978f0d6f4f461e70f55655170e8b87ea47e9
AlejandroMrosas <alejandromrosas@outlook.com>
2021-10-06 22:09:22 -0700

    updated

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,8 +1,9 @@
 var searchEl = document.querySelector('submit');
+var search = document.querySelector("#city").value;
 
 var weatherApp = function(user) {
 
-var api = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=5024011b229f8ff2b79d105f9dc60a26';
+var api = 'https://api.openweathermap.org/data/2.5/weather?' + search + '&id=2172797&appid=5024011b229f8ff2b79d105f9dc60a26';
 
 fetch(api).then(function(response) {
     console.log(response);
@@ -16,7 +17,9 @@ var todayWeather = document.getElementById("select-city");
 
   var weatherTitle = document.createElement("h1");
   var tempSection = document.createElement("p");
-  var icon = document.createElement("img");
+  var humidity = document.createElement('p');
+  var pressure = document.createElement('p')
+  var wind = document.createElement('p')
 
   weatherTitle.textContent = "Weather:"
   todayWeather.appendChild(weatherTitle);
@@ -24,8 +27,14 @@ var todayWeather = document.getElementById("select-city");
   tempSection.textContent = "Temperature: " + data.main.temp;
   todayWeather.appendChild(tempSection);
 
-  icon.textContent = "http://openweathermap.org/img/w/" + icon + ".png";
-  todayWeather.appendChild(icon);
+  humidity.textContent = "Humidity: " + data.main.humidity;
+  todayWeather.appendChild(humidity);
+
+  pressure.textContent = "Pressure: " + data.main.pressure;
+  todayWeather.appendChild(pressure);
+
+  wind.textContent = "Wind: " + data.wind.deg;
+  todayWeather.appendChild(wind);
 
       });
     });
